f381977b02226b143123262075f86bb770b56eb5
Christian Simon <csimon1127@gmail.com>
2020-10-01 18:26:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,18 +1,18 @@
 # Your code goes here!
 class Anagram 
-  attr_accessor :words
+  attr_accessor :word
   
-  def initialize(words)
-    @words = words
+  def initialize(word)
+    @word = word
   end 
   
-  def match(word)
+  def match(words)
     ret_arr = []
     counter = 0
-    @words.each { |anagram|
+    words.each { |anagram|
       if anagram.length == word.length 
         temp_word = anagram.split(//).sort
-        compare = word.split(//).sort
+        compare = @word.split(//).sort
         if compare == temp_word
           ret_arr << anagram
         end
